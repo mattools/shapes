3dbbc42df5c60368e8f7829050707f4abcdadbd0
dlegland <david.legland@nantes.inra.fr>
2018-09-01 18:57:36 +0200

    add tests

---
+++ tests/test_Polygon2D.m
@@ -0,0 +1,48 @@
+function testSuite = test_Polygon2D(varargin)
+%TEST_POLYGON2D  Test case for the file Polygon2D
+%
+%   Test case for the file Polygon2D
+
+%   Example
+%   test_Polygon2D
+%
+%   See also
+%
+
+% ------
+% Author: David Legland
+% e-mail: david.legland@nantes.inra.fr
+% Created: 2018-09-01,    using Matlab 8.6.0.267246 (R2015b)
+% Copyright 2018 INRA - Cepia Software Platform.
+
+testSuite = buildFunctionHandleTestSuite(localfunctions);
+
+function test_Constructor %#ok<*DEFNU>
+% Test call of function without argument
+
+vertices = [10 10; 20 10; 20 20; 10 20];
+poly = Polygon2D(vertices);
+
+
+function test_perimeter_square %#ok<*DEFNU>
+% Test call of function without argument
+
+vertices = [10 10; 20 10; 20 20; 10 20];
+poly = Polygon2D(vertices);
+exp = 40;
+
+perim = perimeter(poly);
+
+assertEqual(exp, perim, .01);
+
+function test_area_square %#ok<*DEFNU>
+% Test call of function without argument
+
+vertices = [10 10; 20 10; 20 20; 10 20];
+poly = Polygon2D(vertices);
+exp = 100;
+
+a = area(poly);
+
+assertEqual(exp, a, .01);
+
